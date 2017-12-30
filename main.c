0a5964961a160f811c11a7364dee3fd4a119bc59
johnmrinderer <johnmrinderer@lewisu.edu>
2017-12-30 15:38:31 -0600

    Scanner - Main

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+int MAX = 8;
+
 int main(int argc, char *argv[]) {
     FILE *filePtr;
 
@@ -38,6 +40,28 @@ int main(int argc, char *argv[]) {
 
     int chrCount = 0;
     char str[MAX];
+    int i;
+
+    int isVald = 1;
+    while ((c=fgetc(filePtr))!= EOF){
+        if(c =='\n'){
+            curntLne++;
+        }
+        if (c=='/'){
+            if(fgetc(filePtr)=='/') {
+                while ((c = fgetc(filePtr)) != '\n') {
+                    curntLne++;
+                }
+            }
+        }if (isalnum(c)){
+            str[chrCount]=c;
+            chrCount++;
+            if (chrCount > MAX){
+                printf("Word %s on line # %d is outside the size limitation\n",str,curntLne);
+                exit(1);
+            }
+        }
+    }
 
     return 0;
 }
